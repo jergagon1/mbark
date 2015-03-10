16b693fdbc287f1b6a8f4cf0d695eb0a76c09c0b
Jeremy Gagon <jergagon1@gmail.com>
2015-03-09 18:57:25 -0700

    progress on routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,21 +1,46 @@
 # GET ===========================================================
 
+#render home page
 get '/' do
-  #login page route
   erb :index
 end
 
+# render sign-in partial
+# get '/sessions/new' do
+#   erb :_sign_in
+# end
 
+# # render sign-up partial
+# get '/users/new' do
+#   erb :_sign_up
+# end
 
 
 # POST ===========================================================
 
+# sign-in
+post '/sessions' do
+  @current_user = User.find_by(email: params[:email])
+  p @current_user
+  if @current_user && @current_user.authenticate(params[:password])
+    session[:user_id] = @current_user.id
+    redirect '/'
+  else
+    redirect '/sessions/new'
+  end
+end
 
 
-
-
-
-
+# sign-up a new user
+post '/users' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id]=user.id
+  else
+    redirect '/users/new'
+  end
+  redirect '/'
+end
 
 # PUT ===========================================================
 
@@ -29,5 +54,10 @@ end
 # DELETE ===========================================================
 
 
+# sign-out -- invoked
+delete '/sessions/:id' do
+  session[:user_id]=nil
+  redirect '/'
+end
 
 
